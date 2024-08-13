4eba7fc6969a10678c4ac203adc71e4842bcef3a
Luis Eduardo <luiseduardo20.dev@gmail.com>
2024-08-13 11:00:29 -0300

    :tada: First Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <HelloWorld msg="teste" />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>teste navegação</h2>
+</template>
