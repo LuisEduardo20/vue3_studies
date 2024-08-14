49663d81be489e9f3af9cec3c29b6bb0f6c942f1
Luis Eduardo <luiseduardo20.dev@gmail.com>
2024-08-14 14:31:42 -0300

    style: default global style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <HelloWorld msg="teste" />
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
